7a1d0bdf140faf88bc7da313ed31ffe59b2211c3
Derick Sayavong <dsayavong54651@gmail.com>
2023-09-14 23:00:05 -0600

    start handler method

--- timer.py
+++ timer.py
@@ -22,19 +22,48 @@ class App(ctk.CTk):
 		#* DATA
 
 		#* WIDGETS
-		self.control_buttons = ControlButtons(self, self.button_fonts)
-
+		self.control_buttons = ControlButtons(
+			self, 
+			self.button_fonts,
+			start = self.start,
+			pause = self.pause,
+			resume = self.resume,
+			reset = self.reset,
+			create_lap = self.create_lap)
 
 		#* RUN
 		self.mainloop()
 
 
+	def start(self):
+		print('start')
+
+	def pause(self):
+		print('pause')
+
+	def resume(self):
+		print('resume')
+
+	def reset(self):
+		print('reset')
+
+	def create_lap(self):
+		print('lap')
 
 
 class ControlButtons(ctk.CTkFrame): #! frame for all the buttons
-	def __init__(self, parent, button_font):
+	def __init__(self, parent, button_font, start, pause, resume, reset, create_lap):
 		super().__init__(parent, corner_radius = 0, fg_color = 'transparent')
 		self.grid(row = 1, column = 0, sticky = 'news')
+
+		#* INTERACTION METHODS
+		self.start = start
+		self.pause = pause
+		self.resume = resume
+		self.reset = reset
+		self.create_lap = create_lap
+		self.state = 'OFF'
+		
 
 		#* GRID LAYOUT
 		self.rowconfigure(0, weight = 1, uniform = 'b')
@@ -59,13 +88,27 @@ class ControlButtons(ctk.CTkFrame): #! frame for all the buttons
 		self.start_button = ctk.CTkButton( #! start button
 			self, 
 			text = 'Start',
-			command = lambda: print('start'),
+			command = self.start_handler,
 			fg_color = GREEN,
 			hover_color = GREEN_HIGHLIGHT,
 			text_color = GREEN_TEXT,
 			font = button_font)
 		
 		self.start_button.grid(row = 0, column = 3, columnspan = 1, sticky = 'news')
+
+	def start_handler(self): #! handles the start, pause, and resume methods
+		if self.state == 'OFF':
+			self.start()
+			self.state = 'ON'
+
+		elif self.state == 'ON':
+			self.pause()
+			self.state == 'PAUSE'
+
+		elif self.state == 'PAUSE':
+			self.resume()
+			self.state = 'ON'
+
 
 if __name__ == '__main__':
 	App()
